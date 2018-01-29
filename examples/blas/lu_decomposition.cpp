e6787fa40607ccf4b850cef093e7dab23fdd3dc0
Ravenwater <theo@stillwater-sc.com>
2018-01-28 19:03:21 -0500

    WIP: Adding a Kalman filter benchmark

--- examples/blas/lu_decomposition.cpp
+++ examples/blas/lu_decomposition.cpp
@@ -1,6 +1,6 @@
 // lu_decomposition.cpp example program comparing float vs posit equation solver
 //
-// Copyright (C) 2017 Stillwater Supercomputing, Inc.
+// Copyright (C) 2017-18 Stillwater Supercomputing, Inc.
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 
@@ -16,7 +16,7 @@
 using namespace std;
 using namespace sw::unum;
 
-// Turn it of for now
+// Turn it off for now
 #define USE_POSIT
 
 int main(int argc, char** argv)
